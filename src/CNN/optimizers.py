092b90a0da2aefe0eaad9056a16ee564031fa35e
Lam Nguyen <tunglam030699@gmail.com>
2022-12-13 15:15:52 -0500

    cnn

--- src/CNN/optimizers.py
+++ src/CNN/optimizers.py
@@ -14,7 +14,7 @@ class Optimizer:
         if name != None and name in keys:
             self.opt_dict[name](layers=layers, training=False)
 
-     def adam(self, layers, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8, decay=0, training=True):
+    def adam(self, layers, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8, decay=0, training=True):
         for l in layers:
             if l.parameters != 0:
                 if training:
@@ -22,20 +22,24 @@ class Optimizer:
                     if l.t == 1:
                         l.pdelta_biases = 0
                         l.pdelta_weights = 0
-                    l.weights_adam1 = beta1 * l.weights_adam1 + (1-beta1)*l.delta_weights
-                    l.weights_adam2 = beta2 * l.weights_adam2 + (1-beta2)*(l.delta_weights**2)
+                    l.weights_adam1 = beta1 * l.weights_adam1 + \
+                        (1-beta1)*l.delta_weights
+                    l.weights_adam2 = beta2 * l.weights_adam2 + \
+                        (1-beta2)*(l.delta_weights**2)
                     mcap = l.weights_adam1/(1-beta1**l.t)
                     vcap = l.weights_adam2/(1-beta2**l.t)
                     l.delta_weights = mcap/(np.sqrt(vcap) + epsilon)
                     l.weights += l.pdelta_weights * self.mr + learning_rate * l.delta_weights
                     l.pdelta_weights = l.delta_weights * 0
-                    l.biases_adam1 = beta1 * l.biases_adam1 + (1-beta1)*l.delta_biases
-                    l.biases_adam2 = beta2 * l.biases_adam2 + (1-beta2)*(l.delta_biases**2)
+                    l.biases_adam1 = beta1 * l.biases_adam1 + \
+                        (1-beta1)*l.delta_biases
+                    l.biases_adam2 = beta2 * l.biases_adam2 + \
+                        (1-beta2)*(l.delta_biases**2)
                     mcap = l.biases_adam1/(1-beta1**l.t)
                     vcap = l.biases_adam2/(1-beta2**l.t)
-                    l.delta_biases = mcap/(np.sqrt(vcap) +epsilon)
+                    l.delta_biases = mcap/(np.sqrt(vcap) + epsilon)
                     l.biases += l.pdelta_biases * self.mr + learning_rate * l.delta_biases
-                    l.pdelta_biases = l.delta_biases * 0                    
+                    l.pdelta_biases = l.delta_biases * 0
                 else:
                     l.t = 0
                     l.weights_adam1 = 0
